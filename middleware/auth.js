33003e6a5d2389b15ed3228126685685087459bd
leobrian67782-lab <leobrian67782@gmail.com>
2026-06-21 09:27:21 +0100

    Add files via upload

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,1 +1,14 @@
+function requireAdmin(req, res, next) {
+  if (req.session && req.session.adminId) {
+    return next();
+  }
+  return res.redirect('/admin/login');
+}
 
+// Makes admin login state available to all EJS views without passing manually each time
+function exposeAdminState(req, res, next) {
+  res.locals.isAdmin = !!(req.session && req.session.adminId);
+  next();
+}
+
+module.exports = { requireAdmin, exposeAdminState };
